92afd1dec6909e388e19cf4a5449911574344d5c
Jay Prakash Maurya <47723510+Jay-2019@users.noreply.github.com>
2020-05-29 00:53:30 +0530

    Update courseFeeType.jsx

--- src/components/courseFeeType/courseFeeType.jsx
+++ src/components/courseFeeType/courseFeeType.jsx
@@ -28,7 +28,6 @@ export default function CourseFeeType(props) {
 
   const calculateFee = values => {
     values.totalFee =
-      values.delayFee +
       values.studyTripFee +
       values.tuitionFee +
       values.laboratory +
